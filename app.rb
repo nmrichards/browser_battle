efe2c67f7f405ff2c285728566e95c8320cd858b
Emma Sjöström <emma.sjoestroem@gmail.com>
2016-04-25 18:10:28 +0200

    added cat

--- app.rb
+++ app.rb
@@ -8,3 +8,9 @@ end
 get '/secret' do
   'This is supposed to be secret! Go away!'
 end
+
+get '/cat' do
+  "<div style='border: 3px dashed red'>
+     <img src='http://bit.ly/1eze8aE'>
+   </div>"
+end
